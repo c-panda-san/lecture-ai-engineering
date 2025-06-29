0f1d64280c028e3e9fe6279a8877d2e93b503815
c-panda-san <tatsuro.yokoi@gmail.com>
2025-06-30 06:04:29 +0900

    Delete sns_analyzer directory

--- sns_analyzer/tests/test_config.py
+++
@@ -1,9 +0,0 @@
-# sns_analyzer/tests/test_config.py
-
-import sns_analyzer.scr.config as config
-
-
-def test_config_values_exist():
-    assert hasattr(config, "REDDIT_CLIENT_ID")
-    assert hasattr(config, "REDDIT_CLIENT_SECRET")
-    assert hasattr(config, "REDDIT_USER_AGENT")
